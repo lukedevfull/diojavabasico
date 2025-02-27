dc849c0c060cc12a76d0ba7428726df8f074dead
Luke_DEV <contato.prof2@gmail.com>
2025-02-26 23:24:46 -0300

    fix TypesOfVar.java

---
+++ firstJavaProject/src/edu/luke/typesOfVar/TypesVar.java
@@ -0,0 +1,16 @@
+package edu.luke.typesOfVar;
+
+public class TypesVar {
+    public static void main(String[] args) {
+        int x = 10; // 32 bits Preferred
+        long y = 1000l; // 64 bits Necessary for biggest numbers, every place L at the end
+        float z = 10.5f; // 32 bits every place F at the end
+        double w = 10.5; // 64 bits Preferred
+        char c = 'a'; // 16 bits
+        boolean b = true; // 1 bit
+
+
+        //FINAL VARIABLES
+        final double VALUE_OF_PI = 3.141592653589793238462643383279502884;
+    }
+}
