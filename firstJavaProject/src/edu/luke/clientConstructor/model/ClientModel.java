ad41362d90d5f77f3420a4c0cfec713a539e0712
Lukedevfull <contato.prof2@gmail.com>
2025-03-23 23:22:41 -0300

    create ClientModel

---
+++ firstJavaProject/src/edu/luke/clientConstructor/model/ClientModel.java
@@ -0,0 +1,15 @@
+package edu.luke.clientConstructor.model;
+
+public class ClientModel {
+  private String name;
+  private String email;
+  private String phone;
+  private String address;
+  private String city;
+  private String state;
+  private String zipCode;
+
+  public ClientModel ( String name, String email){
+    
+  }
+}
